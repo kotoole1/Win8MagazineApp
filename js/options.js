2d91b486ca02bf89b658106c3a5163e01bec8b66
kotoole1 <kevin.otoole@students.olin.edu>
2013-06-18 17:58:55 -0300

    Update options.js

--- js/options.js
+++ js/options.js
@@ -4,15 +4,15 @@
     c1metro.setOptions(
         function () {
             c1metro.options = {
-                appTitle: "Composite C1 Magazine App",                  // App title text
-                dataHostUrl: "http://community.composite.net/",         // Host part of URL where data is downloaded from
-                websiteUrl: "http://www.composite.net/",                // Your website URL
-                privacyUrl: "http://www.composite.net/Privacy",         // Privacy URL - your app won't be accepted by MS if this is not in place 
+                appTitle: "BOSEbuild test page",                         // App title text
+                dataHostUrl: "http://bbcms2.azurewebsites.net/",         // Host part of URL where data is downloaded from
+                websiteUrl: "http://bbcms2.azurewebsites.net/",          // Your website URL
+                privacyUrl: "http://bbcms2.azurewebsites.net/Privacy",   // Privacy URL - your app won't be accepted by MS if this is not in place 
 
                 // Info to share (share charm) when not showing a content item with a url
-                genericShareUrl: "http://www.composite.net/",
-                genericShareTitle: "Composite C1",
-                genericShareSubTitle: "Manage your content for web and devices with this awesome open source CMS...",
+                genericShareUrl: "http://bbcms2.azurewebsites.net/",
+                genericShareTitle: "BOSEbuild",
+                genericShareSubTitle: "A random test website",
 
                 // Cache setting / off-line mode messages
                 cacheRefreshDelay: 1000,                                    // ms to wait before server is contacted to refresh cached groups/content
